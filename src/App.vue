d79ffc456b2028fef9d1b4242f256ad704d09448
Helens-dot <374961015@qq.com>
2023-03-30 11:06:22 +0800

    test-first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<test></test>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {ref} from 'vue'
+import cam from './views/cam.vue'
+import test from './views/test.vue'
+const tackPhoto = ref(false);
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    //cam
+	test
   }
 });
 </script>
---
+++ src/views/cam.vue
@@ -0,0 +1,25 @@
+<template>
+ <div style="text-align: center;">
+     <button style="float: left;" class="btn btn-primary">Left Button</button>
+     <button class="btn btn-primary">Center Button</button>
+     <button style="float: right;" class="btn btn-primary">Right Button</button>
+ </div>
+
+</template>
+
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+</style>
